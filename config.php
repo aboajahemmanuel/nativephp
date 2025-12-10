18761bc18f3f78721492d36529cc7b70f6918155
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-12-10 18:17:08 +0100

    DN

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ return [
     'mysql' => [
         'host' => '127.0.0.1',
         'port' => 3306,
-        'dbname' => 'nativephp3_1765378167',
+        'dbname' => 'nativephp',
         'user' => 'root',
         'pass' => '',
         'charset' => 'utf8mb4',
